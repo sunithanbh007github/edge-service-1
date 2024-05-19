e7c7b0381a8aa6847bf997ca4d4d81d9264b186d
nvasireddygithub <naveen.hbs007@gmail.com>
2024-05-19 14:31:45 +0530

    added security

---
+++ src/main/java/com/polarbookshop/edgeservice1/user/UserController.java
@@ -0,0 +1,25 @@
+package com.polarbookshop.edgeservice1.user;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @GetMapping("user")
+    public Mono<User> getUser(@AuthenticationPrincipal OidcUser oidcUser) {
+        var user = new User(
+                oidcUser.getPreferredUsername(),
+                oidcUser.getGivenName(),
+                oidcUser.getFamilyName(),
+                oidcUser.getClaimAsStringList("roles")
+        );
+        return Mono.just(user);
+    }
+}
