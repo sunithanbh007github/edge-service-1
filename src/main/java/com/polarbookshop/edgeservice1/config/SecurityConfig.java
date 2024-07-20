c5cf9d3e32edafba520dd6173eba98e94a4b25e9
nvasireddygithub <naveen.hbs007@gmail.com>
2024-07-20 22:10:37 +0530

    added kustomize

--- src/main/java/com/polarbookshop/edgeservice1/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edgeservice1/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
             ServerHttpSecurity http, ReactiveClientRegistrationRepository clientRegistrationRepository) {
         return http
                 .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/actuator/**").permitAll()
                         .pathMatchers("/", "/*.css", "/*.js", "/favicon.ico").permitAll()
                         .pathMatchers(HttpMethod.GET, "/books/**").permitAll()
                         .anyExchange().authenticated())
